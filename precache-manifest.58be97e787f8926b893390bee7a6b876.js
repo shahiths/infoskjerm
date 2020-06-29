878f6e8d143bc5f567cdecd0a64021f22ef0fce4
Shahitha Sothinathan <shahitha.sothinathan@Shahithas-MacBook-Pro.local>
2020-06-29 13:47:51 +0200

    Updates

---
+++ precache-manifest.58be97e787f8926b893390bee7a6b876.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8147d984530ad6e20f6984742c443fd7",
+    "url": "/infoskjerm/index.html"
+  },
+  {
+    "revision": "e8af71d338295bc319eb",
+    "url": "/infoskjerm/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8de876d35368ec49b950",
+    "url": "/infoskjerm/static/js/2.12fd2ae6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/infoskjerm/static/js/2.12fd2ae6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8af71d338295bc319eb",
+    "url": "/infoskjerm/static/js/main.081e5737.chunk.js"
+  },
+  {
+    "revision": "6fa4fe89daa3d5d07bae",
+    "url": "/infoskjerm/static/js/runtime-main.d6c9a5ab.js"
+  }
+]);
